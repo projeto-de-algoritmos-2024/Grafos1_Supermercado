bf7a7e0a33309e7e6491b589d4326c680ca71537
Brunna Louise Martins <brunna.louisemr@gmail.com>
2024-10-28 16:48:27 -0300

    adiciona bfs
    Co-authored-by: pkbceira03 <cabeceira2003@gmail.com>

--- supermecado.py
+++ supermecado.py
@@ -1,13 +1,41 @@
+from collections import deque
+
 class Grafo:
 
+    # inicializa grafo
     def __init__(self, grafo=None):
+        # inicializa novo grafo vazio se não encontrar grafo existente
         if grafo is None:
             grafo = {}
+        # grafo = dicionário já criado
         self.grafo = grafo
 
     def mostra_grafo(self):
+        # printa o dicionário que contém as conexões entre cada vértice, como uma lista encadeada
         print(self.grafo)
+        # printa qtd de vertices
         print(len(self.grafo))
+
+    def busca_largura(self, inicio, fim):
+        arvore_busca = []
+        visitados = []
+        fila = deque([inicio])
+        fila.append(inicio)
+        visitados.append(inicio)
+        while (fila):
+            vertice = fila.popleft()
+            for vizinho in self.grafo.get(vertice, []):
+                if vizinho == fim:
+                    arvore_busca.append((vertice, vizinho))
+                    visitados.append(vizinho)
+                    return arvore_busca
+                elif not (vizinho in visitados):
+                    arvore_busca.append((vertice, vizinho))
+                    visitados.append(vizinho)
+                    fila.append(vizinho)
+
+
+
 
 meu_grafo = {
     1:[2,3],
@@ -45,7 +73,7 @@ meu_grafo = {
     33:[15,16,17,32,34],
     34:[14,15,16,33,35],
     35:[14,15,34,37],
-    36:[37,40,63],
+    36:[37,40,64],
     37:[2,35,36],
     38:[39,49,62],
     39:[24,26,38],
@@ -88,4 +116,6 @@ meu_grafo = {
 
 grafo = Grafo(meu_grafo)
 
-grafo.mostra_grafo()
+#grafo.mostra_grafo()
+
+print(grafo.busca_largura(1,60))
